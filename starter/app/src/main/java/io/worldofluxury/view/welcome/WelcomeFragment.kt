15d4d4eae0a034d997eb2b0c3501b2c7608c05b8
Dennis Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-08-01 20:48:30 +0000

    Added auth page

--- starter/app/src/main/java/io/worldofluxury/view/welcome/WelcomeFragment.kt
+++ starter/app/src/main/java/io/worldofluxury/view/welcome/WelcomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
 import io.worldofluxury.R
 
 
@@ -16,6 +18,14 @@ class WelcomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<MaterialButton>(R.id.get_started).setOnClickListener {
+            findNavController().navigate(R.id.action_nav_welcome_to_nav_auth)
+        }
     }
 
 }
